9d5d52e7cb618006fffc6d4102fc254cb10478c3
lb <lb@bcax365.com>
2019-09-01 23:22:37 +0800

    201919_09_01

---
+++ lib/bowling_game/2019_08_24/game3_test .dart
@@ -0,0 +1,59 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kaka/bowling_game/2019_08_24/game1.dart';
+
+void main() {
+  group('Game Test', () {
+    Game g = new Game();
+
+    void rollMany(int n, int pins) {
+      for (var i = 0; i < n; i++) {
+        g.roll(pins);
+      }
+    }
+
+    void rollSpare() {
+      g.roll(5);
+      g.roll(5);
+    }
+
+    void rollStrike() {
+      g.roll(10);
+    }
+
+    test('testGutterGame', () {
+      rollMany(20, 0);
+
+      assert(g.score() == 0);
+    });
+
+    test('testAllOnes', () {
+      rollMany(20, 1);
+
+      expect(g.score(), 20);
+    });
+
+    test('testOneSpare', () {
+      rollSpare();
+      g.roll(3);
+      rollMany(17,0);
+
+      assert(g.score() == 16);
+    });
+
+    test('testOneStrike', () {
+      rollStrike();
+      g.roll(3);
+      g.roll(4);
+      rollMany(16, 0);
+
+      assert(g.score() == 24);
+    });
+
+    test('testPerfectGame', () {
+      rollMany(12,10);
+
+      expect(g.score(), 300);
+    });
+  });
+  
+}
---
+++ lib/bowling_game/2019_08_31/game2.dart
@@ -0,0 +1,50 @@
+class Game {
+  final List<int> rolls = new List(21);
+
+  int currentRoll = 0;
+
+  void roll(int pins) {
+    rolls[currentRoll ++] = pins;
+  }
+
+  int score() {
+    int score = 0;
+
+    int frameIndex = 0;
+
+    for (int frame = 0; frame < 10; frame++) {
+      if (isStrike(frameIndex)) {
+        score += 10 + strikeBonus(frameIndex);
+        frameIndex++;
+      } else if (isSpare(frameIndex)) {
+        score += 10 + spareBonus(frameIndex);
+        frameIndex += 2;
+      } else {
+        score += sumOfBallsInFrame(frameIndex);
+        frameIndex += 2;
+      }
+    }
+
+    return score;
+  }
+
+  bool isStrike(int frameIndex) {
+    return rolls[frameIndex] == 10;
+  }
+
+  bool isSpare(int frameIndex) {
+    return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+  }
+
+  int strikeBonus(int frameIndex) {
+    return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+  }
+
+  int spareBonus(int frameIndex) {
+    return rolls[frameIndex + 2];
+  }
+
+  int sumOfBallsInFrame(int frameIndex) {
+    return rolls[frameIndex] + rolls[frameIndex + 1];
+  }
+}
